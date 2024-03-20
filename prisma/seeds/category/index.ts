fdb826786c96c9fb561afb73f4c8fb71a44f14b9
Guilherme Nogueira <guilhermenogueira90@gmail.com>
2024-03-20 19:05:09 -0300

    feature: ajustes nos seeders, package.json e algumas mudanças no schema.prisma

--- prisma/seeds/category/index.ts
+++ prisma/seeds/category/index.ts
@@ -7,47 +7,47 @@ export async function expensesCategorySeed(prisma: PrismaClient): Promise<void> 
     {
       name: 'Alimentação',
       slug: 'alimentacao',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Assinaturas',
       slug: 'assinaturas',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Casa',
       slug: 'casa',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Supermercado',
       slug: 'supermercado',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Cuidados pessoais',
       slug: 'cuidados-pessoais',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Dívidas e empréstimos',
       slug: 'dividas-e-emprestimos',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Educação',
       slug: 'educacao',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Investimentos',
       slug: 'investimentos',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Lazer',
       slug: 'lazer',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Saúde',
       slug: 'saude',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },{
       name: 'Transporte',
       slug: 'transporte',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     },
   ]
 
@@ -63,11 +63,11 @@ export async function incomesCategorySeed(prisma: PrismaClient): Promise<void> {
     {
       name: 'Investimento',
       slug: 'investimento',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     }, {
       name: 'Salário',
       slug: 'salario',
-      type_of_transaction_id: typeOfTransaction.id
+      type_transaction_id: typeOfTransaction.id
     }
   ]
 
